33fb50febb4ed544b57b9cb73a92352c571a1999
mizack mianuti <mizack1234@gmail.com>
2024-07-26 22:52:47 -0300

    validate config file

--- app/connection_db.py
+++ app/connection_db.py
@@ -1,2 +1,84 @@
 class Connection:
-    pass
+    def __init__(self, data_connection:dict, replicated_connection = False) -> None:
+        self.replicated_connection = replicated_connection
+
+        self.host = data_connection.get("HOST")
+        self.port = data_connection.get("PORT")
+        self.user = data_connection.get("USER")
+        self.password = data_connection.get("PASSWORD")
+        self.database = data_connection.get("DATABASE")
+        self.table = data_connection.get("TABLE")
+        self.data = data_connection.get("DATA", False)
+        self.dbms = data_connection.get("DBMS")
+
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @host.setter
+    def host(self, host):
+        self._host = host
+
+    
+    @property
+    def port(self) -> str:
+        return self._port
+
+    @port.setter
+    def port(self, port):
+        self._port = port
+
+    
+    @property
+    def user(self) -> str:
+        return self._user
+
+    @user.setter
+    def user(self, user):
+        self._user = user
+
+    
+    @property
+    def password(self) -> str:
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    
+    @property
+    def database(self) -> list:
+        return self._database
+
+    @database.setter
+    def database(self, database):
+        self._database = database
+
+    
+    @property
+    def table(self) -> list:
+        return self._table
+
+    @table.setter
+    def table(self, table):
+        self._table = table
+
+    
+    @property
+    def data(self) -> bool:
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        self._data = data
+    
+
+    @property
+    def dbms(self) -> str:
+        return self._dbms
+
+    @dbms.setter
+    def dbms(self, dbms):
+        self._dbms = dbms
--- app/replicator.py
+++ app/replicator.py
@@ -5,35 +5,58 @@ from app.connection_db import Connection
 
 class Replicator:
     def __init__(self, config_file = "connections.json") -> None:
-        self.config_file = config_file
-        self.__validate_config_file()
+        self.config_file = None
+        self.file_path = None
+        self.replicated_connection = None
+        self.other_connections = []
+
+        self.__validate_config_file(config_file)
 
 
     @property
-    def config_file(self):
+    def config_file(self) -> dict:
         return self._config_file
 
     @config_file.setter
     def config_file(self, config_file):
         self._config_file = config_file
+    
+
+    def __validate_config_file(self, config_file):
+        '''
+        Verify if config file exists and install it.
+        '''
+        self.__verify_if_config_file_exists(config_file)
+        self.__load_configs_databases()
+        self.__check_json_fields()
+        
+    
+    def __verify_if_config_file_exists(self, config_file):
+        current_directory = os.getcwd()
+        self.file_path = f"{current_directory}\config\{config_file}"
+
+        if not os.path.exists(self.file_path):
+            raise ValueError("Configuration file not found")
+        
+
+    def __load_configs_databases(self):
+        try:
+            file_open = open(self.file_path)
+            self.config_file = json.load(file_open)
+        except Exception as e:
+            raise ValueError(f"Configuration file is invalid {e}")
+        
+    
+    def __check_json_fields(self):
+        self.replicated_connection = Connection(self.config_file.get("replicated_connection"))
+        others_connections = self.config_file.get("other_connections")
+
+        if not isinstance(others_connections, list):
+            raise ValueError("The other connections weren't sent correctly")
+        
+        for connection in others_connections:
+            self.other_connections.append(Connection(connection))
 
 
     def run(self):
         return
-    
-
-    def __validate_config_file(self):
-        '''
-        Verify if config file exists and install it.
-        '''
-        self.__verify_if_config_file_exists()
-        
-    
-    def __verify_if_config_file_exists(self):
-        current_directory = os.getcwd()
-        file_path = f"{current_directory}\config\{self.config_file}"
-
-        if not os.path.exists(file_path):
-            raise ValueError("Configuration file not found")
-        
-    
